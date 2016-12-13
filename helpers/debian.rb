df14df03ee21f8e3511d4f10df5494db55d618fa
Timo Beckers <timo@incline.eu>
2016-12-13 10:53:26 +0100

    Re-order helpers; re-work debian_get_source and debuild methods for use with Rake

--- helpers/debian.rb
+++ helpers/debian.rb
@@ -1,51 +1,60 @@
 require_relative 'generic.rb'
 
-# Deletes all files ending in *.build and *.changes in the recipe directory
-# Cleans out 'builddir' (tmp_build/)
-def debian_cleanup
-  # Remove build output ending in .build or .changes
-  cleanup = Dir.glob(workdir/"*.{build,changes}")
-  log("Deleting the following files after successful build: #{cleanup}")
-  FileUtils.rm(cleanup)
-
-  # Clean up build directory
-  if Dir.exist?(builddir)
-    FileUtils.rm_rf(Dir.glob(builddir/'*'))
-    Log.info "Cleared #{builddir}"
-  end
-end
-
-# Execute apt-get build-dep for the given package
-def debian_build_dep(pkg)
-  Log.info "Running apt-get build-dep for package #{pkg}"
-  sh "DEBIAN_FRONTEND=noninteractive apt-get -y build-dep #{pkg} > /dev/null"
-end
-
 # Download a source package to the cache folder
 # - pkg is the package to download using 'apt-get source'
 # - tar_prefix is the package to look for when conditionally downloading
 #    It is used to generate a glob pattern: <tar_prefix>_*.orig.tar.xz
 #    The default value is set to the value given to 'pkg'
-def debian_get_source(pkg: nil, tar_prefix: pkg)
+def debian_get_source(pkg: nil, tar_prefix: pkg, quiet: true)
   if not pkg
     raise 'pkg parameter must be given to debian_get_source()'
   end
 
+  @pkg ||= pkg
   pattern = "#{tar_prefix}_*.orig.tar.xz"
-  tarball = Dir.glob(cachedir/pattern).last
+  tarball = Dir.glob(cachedir/pkg/pattern).last
+  q = quiet ? '> /dev/null' : ''
 
-  if not Dir.exist?(cachedir)
-    mkdir cachedir
-  end
+  # Ensure cachedir exists
+  cachedir(pkg).mkdir
 
   if not tarball
-    Log.info 'No glob matches found for ' + cachedir/pattern + ', running apt-get source'
+    log 'No glob matches found for ' + cachedir/pkg/pattern
 
-    chdir cachedir
-    sh "apt-get source #{pkg}"
+    # run apt-get source in cache/
+    chdir cachedir/pkg
+    log "Running 'apt-get source' for package #{pkg}"
+    shell %Q{apt-get source #{pkg} #{q}}
+    log "Silencing 'apt-get source' output for package #{pkg}" if quiet == true
   else
-    Log.info "Found source archive at #{tarball}, skipping apt-get source"
+    log "Found source archive at #{tarball}, skipping apt-get source"
   end
+
+  # Ensure builddir exists
+  builddir(pkg).mkdir
+
+  if not extractcookie?(pkg)
+    log "No extract cookie found for package #{pkg}, copying to " + builddir/pkg
+    FileUtils.cp_r "#{cachedir/pkg}/.", builddir/pkg
+    FileUtils.touch builddir/".extractcookie_#{pkg}"
+  else
+    log "Extract cookie found at " + builddir/".extractcookie_#{pkg}" + ", skipping"
+  end
+end
+
+# Executes debuild with the specified number of threads
+def debuild(threads: 4, quiet: true)
+  if not threads.to_s =~ /^[0-9]+$/
+    raise 'amount of threads given to debuild() needs to be an integer'
+  end
+
+  q = quiet == true ? '> /dev/null' : ''
+
+  log "Changing into detected extracted source directory"
+  chdir Dir.glob(builddir/@pkg/"#{@pkg}*/").last
+
+  log "Running debuild for package #{@pkg} with #{threads} threads"
+  shell %Q{DEB_BUILD_OPTIONS=nocheck debuild -us -uc -b -j#{threads} #{q}}
 end
 
 # Moves all *.deb files from the recipe directory to 'pkgdir' (default ./pkg)
@@ -56,19 +65,23 @@ def debian_move_deb
   if output.empty?
     Log.error "No build output found for #{name}, continuing"
   else
-    Log.info "#{name} build output: #{output}"
+    log "#{name} build output: #{output}"
     FileUtils.mv output, pkgdir
-    Log.info "Moved build output to #{pkgdir}"
+    log "Moved build output to #{pkgdir}"
   end
 end
 
-# Executes debuild with the specified number of threads
-def debuild(threads = 4)
-  if not threads.to_s =~ /^[0-9]+$/
-    raise 'amount of threads given to debuild() needs to be an integer'
+# Deletes all files ending in *.build and *.changes in the recipe directory
+# Cleans out 'builddir' (tmp_build/)
+def debian_cleanup
+  # Remove build output ending in .build or .changes
+  cleanup = Dir.glob(workdir/"*.{build,changes}")
+  log("Deleting the following files after successful build: #{cleanup}")
+  FileUtils.rm(cleanup)
+
+  # Clean up build directory
+  if Dir.exist?(builddir)
+    FileUtils.rm_rf(builddir/'*')
+    log "Cleared #{builddir}"
   end
-
-  Log.info "Running debuild with #{threads} threads"
-
-  sh "DEB_BUILD_OPTIONS=nocheck debuild -us -uc -b -j#{threads} > /dev/null"
 end
--- helpers/generic.rb
+++ helpers/generic.rb
@@ -1,12 +1,64 @@
+require 'pathname'
 
-cachedir = 'cache'
-tmpdir = 'tmp-build'
-pkgdir = 'pkg'
+class Path < Pathname
+  if '1.9' <= RUBY_VERSION
+    alias_method :to_str, :to_s
+  end
 
-def /(path)
-  self + (path || '').to_s.gsub(%r{^/}, '')
+  def self.pwd(path = nil)
+    new(Dir.pwd)/path
+  end
+
+  def /(path)
+    self + (path || '').to_s.gsub(%r{^/}, '')
+  end
+
+  def +(other)
+    other = Path.new(other) unless Path === other
+    Path.new(plus(@path, other.to_s))
+  end
+
+  def mkdir
+    if not Dir.exist?(self.to_s)
+      # FileUtils.mkdir_p in Ruby 1.8.7 does not return an array.
+      Array(FileUtils.mkdir_p(self.to_s))
+    end
+  end
+end
+
+def workdir(dir = nil)
+  dir.gsub!(%r{^/}, '') if dir
+
+  # Store the workdir when this method is called for the first time
+  (@workdir ||= Path.pwd)/dir
+end
+
+def pkgdir(path = nil)
+  workdir('pkg')/path
+end
+
+def cachedir(path = nil)
+  workdir('cache')/path
+end
+
+def builddir(path = nil)
+  workdir('tmp-build')/path
+end
+
+def extractcookie?(pkg)
+  File.exist?(builddir/".extractcookie_#{pkg}")
 end
 
 def log(text)
 	puts "--> #{text}"
+end
+
+# From fpm. (lib/fpm/util.rb)
+def shell(*args)
+  # Make sure to avoid nil elements in args. This might happen on 1.8.
+  success = system(*args.compact.flatten)
+  if !success
+    raise "'system(#{args.inspect})' failed with error code: #{$?.exitstatus}"
+  end
+  return success
 end
